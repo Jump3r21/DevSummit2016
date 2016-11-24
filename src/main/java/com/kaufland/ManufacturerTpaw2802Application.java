46f839033876ecd9f95f501d4bf5be85c6003af2
tpaw2802 <thomas.pawlassek@kaufland.com>
2016-11-24 15:10:54 +0100

    Init

---
+++ src/main/java/com/kaufland/ManufacturerTpaw2802Application.java
@@ -0,0 +1,12 @@
+package com.kaufland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManufacturerTpaw2802Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManufacturerTpaw2802Application.class, args);
+	}
+}
